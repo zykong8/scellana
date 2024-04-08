a36d114d0e4d6ddf316d93973afff16584aa2136
pfliu <pfliu@aptbiotech.com>
2024-04-08 17:36:27 +0800

    v1.0.0

---
+++ app/server.R
@@ -0,0 +1,6 @@
+
+server <- function(input, output, session){
+  
+}
+
+shinyServer(server)
---
+++ app/ui.R
@@ -0,0 +1,28 @@
+library(shiny)
+
+ui <- fluidPage(
+  titlePanel("Single Cell Analysis"),
+  
+  navlistPanel(
+    "Figures",
+    tabPanel(
+      "DotPlot",
+      column(
+        width = 4,
+        sliderInput("slider", label = "Test", min = 1, max = 5, value = 3, step = 1),
+        numericInput("cls", label = "Number", value = 5)
+      )
+    ),
+    tabPanel(
+      "Heatmap",
+      column(
+        width = 4,
+        textInput("Gene1", label = "Gene", value = NULL),
+        textAreaInput("GL", label = "Gene list", value = NULL)
+      )
+    )
+  )
+  
+)
+
+shinyUI(ui)
